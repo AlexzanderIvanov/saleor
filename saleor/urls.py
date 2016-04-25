3291825c2cf8e010ab8437b5fcf966267887fbf1
Saleor <alexzander.t.ivanov@gmail.com>
2016-04-25 23:14:31 +0300

    asdad

--- saleor/urls.py
+++ saleor/urls.py
@@ -2,18 +2,18 @@ from django.conf import settings
 from django.conf.urls import url, include
 from django.contrib import admin
 from django.contrib.sitemaps.views import sitemap
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 from django.views.static import serve
 
 from .cart.urls import urlpatterns as cart_urls
 from .checkout.urls import urlpatterns as checkout_urls
 from .core.sitemaps import sitemaps
 from .core.urls import urlpatterns as core_urls
+from .dashboard.urls import urlpatterns as dashboard_urls
 from .order.urls import urlpatterns as order_urls
 from .product.urls import urlpatterns as product_urls
 from .registration.urls import urlpatterns as registration_urls
 from .userprofile.urls import urlpatterns as userprofile_urls
-from .dashboard.urls import urlpatterns as dashboard_urls
-
 
 admin.autodiscover()
 
@@ -30,8 +30,9 @@ urlpatterns = [
     url(r'^selectable/', include('selectable.urls')),
     url(r'^sitemap\.xml$', sitemap, {'sitemaps': sitemaps},
         name='django.contrib.sitemaps.views.sitemap'),
-    url(r'', include('payments.urls'))
+    url(r'', include('payments.urls')),
 ]
+urlpatterns += staticfiles_urlpatterns()
 
 if settings.DEBUG:
     # static files (images, css, javascript, etc.)
