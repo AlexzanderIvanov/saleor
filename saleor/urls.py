52379a8fab19f413117c528c6d39fb02ab0b0edf
Saleor <alexzander.t.ivanov@gmail.com>
2016-05-02 14:03:50 +0300

    adasdad

--- saleor/urls.py
+++ saleor/urls.py
@@ -39,3 +39,7 @@ if settings.DEBUG:
     urlpatterns += [
         url(r'^media/(?P<path>.*)$', serve,
             {'document_root': settings.MEDIA_ROOT})]
+if not settings.DEBUG:
+    urlpatterns += url('',
+        (r'^static/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.STATIC_ROOT}),
+    )
