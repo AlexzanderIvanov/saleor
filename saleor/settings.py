e71addcdfc9be10be3e62837e773d34bd0774ce5
Saleor <alexzander.t.ivanov@gmail.com>
2016-08-10 20:54:54 +0300

    Configured emial server settings. Given some love to email templates.

--- saleor/settings.py
+++ saleor/settings.py
@@ -20,7 +20,7 @@ ROOT_URLCONF = 'saleor.urls'
 WSGI_APPLICATION = 'saleor.wsgi.application'
 
 ADMINS = (
-    # ('Your Name', 'your_email@example.com'),
+    ('Alexander Ivanov', 'contact@ap1shop.com'),
 )
 MANAGERS = ADMINS
 INTERNAL_IPS = os.environ.get('INTERNAL_IPS', '127.0.0.1').split()
