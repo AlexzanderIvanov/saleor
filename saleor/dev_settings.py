77b847ca6b591a1e6dd7a37bc1d03789711059fb
Pavel Aleksandrov <pafffkata@gmail.com>
2016-08-16 00:02:47 +0300

    Added development settings for use on test deploys. Fixed contacts page - breadcrumbs and title now working correctly.

--- saleor/dev_settings.py
+++ saleor/dev_settings.py
@@ -1,10 +1,20 @@
 from .settings import *
+import os.path
+import dj_database_url
 
-SECRET_KEY = 'NOTREALLY'
 
-DEFAULT_CURRENCY = 'USD'
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'ap1shop_test',
+        'USER': 'ap1shop_test',
+        'PASSWORD': 'Ap1forthewin!',
+        'HOST': 'localhost',  # Or an IP Address that your DB is hosted on
+        'PORT': '3306',
+        'default-character-set': 'utf8',`
+    }
+}
 
-DATABASES['default']['TEST'] = {
-    'SERIALIZE': False,
-    'NAME': ':memory:',
-    'MIRROR': None}
+
+EMAIL_URL = os.environ.get('EMAIL_URL', 'console://')
+email_config = dj_email_url.parse(EMAIL_URL)
