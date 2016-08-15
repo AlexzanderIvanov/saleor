36a1fdea01b5c4dd8313a7edd3f5534d1a11682d
Saleor <alexzander.t.ivanov@gmail.com>
2016-08-15 21:49:38 +0300

    setup different environemtns

--- saleor/settings_prod.py
+++ saleor/settings_prod.py
@@ -1,7 +1,6 @@
-from .settings import *
 import os.path
-import dj_database_url
 
+from .settings import *
 
 DATABASES = {
     'default': {
@@ -15,6 +14,14 @@ DATABASES = {
     }
 }
 
-
 EMAIL_URL = 'smtp://orders@ap1shop.com:ap1forthewin@mail.ap1shop.com:25'  # os.environ.get('EMAIL_URL', 'console://')
 email_config = dj_email_url.parse(EMAIL_URL)
+
+GOOGLE_ANALYTICS_TRACKING_ID = os.environ.get('GOOGLE_ANALYTICS_TRACKING_ID', 'UA-82439832-1')
+
+DEFAULT_FROM_EMAIL = 'orders@ap1shop.com'  # os.environ.get('DEFAULT_FROM_EMAIL')
+REPORT_EMAIL = 'orders@ap1shop.com'  # os.environ.get('DEFAULT_FROM_EMAIL')
+
+SECURE_SSL_REDIRECT = True
+SESSION_COOKIE_SECURE = True
+CSRF_COOKIE_SECURE = True
