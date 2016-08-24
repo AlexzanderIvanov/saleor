9be1562db93382a9b097dfcf40e8771131ba3b7e
Saleor <alexzander.t.ivanov@gmail.com>
2016-08-24 20:39:10 +0300

    Enabled translation on shipping office and shipping city names.

--- saleor/shipping/translation.py
+++ saleor/shipping/translation.py
@@ -1,7 +1,17 @@
 from modeltranslation.translator import register, TranslationOptions
-from saleor.shipping.models import ShippingMethod
+from saleor.shipping.models import ShippingMethod, ShippingCity, ShippingOffice
 
 
 @register(ShippingMethod)
 class ShippingMethodTranslationOptions(TranslationOptions):
     fields = ('name', 'description',)
+
+
+@register(ShippingCity)
+class ShippingCityTranslationOptions(TranslationOptions):
+    fields = ('name', )
+
+
+@register(ShippingOffice)
+class ShippingOfficeTranslationOptions(TranslationOptions):
+    fields = ('name', 'address')
